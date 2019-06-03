f3f7b3ad0b35c51a0a5cc730d7baf15a32140c12
Miodrag Brkljac <miodrag.brkljac@powerhouse.nl>
2019-06-03 17:01:39 +0200

    Initialized project using spring initializr

---
+++ src/main/java/com/mbrkljac/afterjavaeight/AfterJavaEightApplication.java
@@ -0,0 +1,13 @@
+package com.mbrkljac.afterjavaeight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AfterJavaEightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AfterJavaEightApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mbrkljac/afterjavaeight/AfterJavaEightApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mbrkljac.afterjavaeight;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AfterJavaEightApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
